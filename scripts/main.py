2e541ad25c79a2822d29c9a731228160a29e4b10
Liu Zhongwei <liuzhongwei@espressif.com>
2024-11-10 16:29:59 +0800

    feat(scripts): support lvgl running on the unix

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,12 @@
-import time
-import os
-import gc
-import _thread
+import lvgl as lv
+import lv_utils
+from display_driver_utils import driver
 
 if __name__ == '__main__':
-    print('Starting main.py')
+    print('Running main.py')
+
+    print('Initializing lvgl')
+    lv.init()
+
+    print('Initializing driver')
+    drv = driver(320, 240)
